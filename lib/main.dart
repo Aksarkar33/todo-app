62a7876d4feb4bb1c5b42b6dd2a69a8d2e0a1103
Aksarkar33 <aksarkar323@gmail.com>
2025-08-18 03:19:05 +0530

    main.dart->add to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 // import 'package:flutter/foundation.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:todo_app/services/auth_services.dart';
+import 'package:todo_app/services/home_screen.dart';
+import 'package:todo_app/signin_screen.dart';
 import 'LoginScreen.dart';
 void main()async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,7 +16,8 @@ void main()async{
 
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+ final FirebaseAuth _auth= FirebaseAuth.instance;
+
 
 
   @override
@@ -21,7 +26,7 @@ class MyApp extends StatelessWidget {
   debugShowCheckedModeBanner:false,
     title: "ToDo",
       theme: ThemeData(primarySwatch:Colors.indigo),
-      home: const LoginScreen(),
+      home: _auth.currentUser!=null?HomeScreen():Loginscreen(),
     );
   }
 }
