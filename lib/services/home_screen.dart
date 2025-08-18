b53ad85661d67e7e369ba8ce17148dc4fddd7809
Aksarkar33 <aksarkar323@gmail.com>
2025-08-18 14:39:24 +0530

    home screen

---
+++ lib/services/home_screen.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_app/LoginScreen.dart';
+import 'package:todo_app/model/todo_model.dart';
+import 'package:todo_app/services/auth_services.dart';
+import 'package:todo_app/services/database_services.dart';
+import 'package:todo_app/widgets/completed_widget.dart';
+import 'package:todo_app/widgets/pending_widget.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _buttonIndex = 0;
+  final _widgets = [
+    PendingWidget(),
+
+    CompletedWidget(),
+
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1d2630),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF1d2630),
+        foregroundColor: Colors.white,
+        title: Text("ToDo"),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await AuthServices().signOut();
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Loginscreen()),
+              );
+            },
+            icon: Icon(Icons.exit_to_app),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 20,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  InkWell(
+                    borderRadius: BorderRadius.circular(10),
+                    onTap: () {
+                      setState(() {
+                        _buttonIndex = 0;
+                      });
+                    },
+                    child: Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width / 2,
+                      decoration: BoxDecoration(
+                        color: _buttonIndex == 0 ? Colors.indigo : Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Pending",
+                          style: TextStyle(
+                            fontSize: _buttonIndex == 0 ? 16 : 14,
+                            fontWeight: FontWeight.w500,
+                            color: _buttonIndex == 0
+                                ? Colors.white
+                                : Colors.black38,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    borderRadius: BorderRadius.circular(10),
+                    onTap: () {
+                      setState(() {
+                        _buttonIndex = 1;
+                      });
+                    },
+                    child: Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width / 2,
+                      decoration: BoxDecoration(
+                        color: _buttonIndex == 1 ? Colors.indigo : Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Completed",
+                          style: TextStyle(
+                            fontSize: _buttonIndex == 1 ? 16 : 14,
+                            fontWeight: FontWeight.w500,
+                            color: _buttonIndex == 1
+                                ? Colors.white
+                                : Colors.black38,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 30),
+            _widgets[_buttonIndex],
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.indigo,
+        onPressed: () {
+          _showTaskDialog(context);
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 28,
+        ),
+      ),
+    );
+  }
+
+  void _showTaskDialog(BuildContext context, {Todo? todo}) {
+    final TextEditingController _titleController = TextEditingController(
+      text: todo?.title,
+    );
+    final TextEditingController _descriptionController = TextEditingController(
+      text: todo?.description,
+    );
+    final DatabaseService _databaseServices = DatabaseService();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          title: Text(
+            todo == null ? "Add Task" : "Edit Task",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          content: SingleChildScrollView(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _titleController,
+                    decoration: InputDecoration(
+                      labelText: "Title",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    controller: _descriptionController,
+                    decoration: InputDecoration(
+                      labelText: "Description",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Cancel"),
+            ),
+
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.indigo,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () async {
+                if (todo == null) {
+                  await _databaseServices.addTodoItem(
+                    _titleController.text,
+                    _descriptionController.text,
+                  );
+                } else {
+                  await _databaseServices.updateTodo(
+                    todo.id,
+                    _titleController.text,
+                    _descriptionController.text,
+                  );
+                }
+                Navigator.pop(context);
+              },
+              child: Text(todo == null ? "Add" : "Update"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
